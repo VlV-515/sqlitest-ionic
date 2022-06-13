a748079c9daad88dc25134f4e8f11d38c7f8c67c
VlV <email@email.com>
2022-06-13 12:18:25 -0500

    Funciono =) xD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SQLite],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  response: any = 'msg Response';
+  constructor(private sqlite: SQLite, private platform: Platform) {}
+  ngOnInit() {
+    this.platform.ready().then((ready) => {
+      if (ready) {
+        this.sqlite
+          .create({
+            name: 'data.db',
+            location: 'default',
+          })
+          .then((db: SQLiteObject) => {
+            db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+              .then(() => (this.response = 'Executed SQL yupiiii'))
+              .catch((e) => (this.response = e));
+          })
+          .catch((e) => console.log(e));
+      }
+    });
+  }
 }
